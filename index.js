506eb8f105e9012df001f82305cbb6e30bde2ca5
Codeaddictjaver <102610715+HatimDev-HE@users.noreply.github.com>
2023-01-28 13:17:36 +0300

    node api

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ const url = require('url');
 ///////////////////////////////////////////////////////////////////////////
 
 // SERVER //////////////
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     
     const pathName = req.url;
@@ -30,14 +34,10 @@ const server = http.createServer((req, res) => {
     } else if (pathName === "/product") {
         res.end("This is the product");
     } else if (pathName === "/api") {
-        fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-            const productData = JSON.parse(data);
-            console.log(productData);
-            res.writeHead(200, {
-                "Content-type": "application/json"
-            });
-            res.end(data);
-        })
+        res.writeHead(200, {
+            "Content-type": "application/json"
+        });
+        res.end(data);
     } else {
         res.writeHead(404, {
             "Content-type": "text/html",
