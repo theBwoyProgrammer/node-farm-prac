50168d182715d1fecbecb369d259574885fb86c9
Codeaddictjaver <102610715+HatimDev-HE@users.noreply.github.com>
2023-01-28 13:05:18 +0300

    node api

--- index.js
+++ index.js
@@ -29,6 +29,21 @@ const server = http.createServer((req, res) => {
         res.end("This is the overview");
     } else if (pathName === "/product") {
         res.end("This is the product");
+    } else if (pathName === "/api") {
+        fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+            const productData = JSON.parse(data);
+            console.log(productData);
+            res.writeHead(200, {
+                "Content-type": "application/json"
+            });
+            res.end(data);
+        })
+    } else {
+        res.writeHead(404, {
+            "Content-type": "text/html",
+            "my-own-header": "hello-world"
+        });
+        res.end("<h1>Page not found!</h1>");
     }
 });
 
